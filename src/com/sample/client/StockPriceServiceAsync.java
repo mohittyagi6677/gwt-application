ad38a08d63d2774af0a55a3c47054a6e0e832c9b
Aditya <hiitsadityakumar@gmail.com>
2018-07-10 16:49:22 +0530

    INITIAL

---
+++ src/com/sample/client/StockPriceServiceAsync.java
@@ -0,0 +1,9 @@
+package com.sample.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface StockPriceServiceAsync {
+
+  void getPrices(String[] symbols, AsyncCallback<StockPrice[]> callback);
+
+}
